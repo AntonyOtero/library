ad57ea0f100e993ea767f664f62b82e44a506a23
= <=>
2021-10-22 08:53:53 -0400

    Bugfix - Convert string values into boolean
    - Fixes #3
    - Using the New Book form now correctly assigns a boolean value
      to the new Book object's read property instead of a string

--- js/library.js
+++ js/library.js
@@ -13,7 +13,7 @@ function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
-  this.read = read
+  this.read = read === 'true'
 }
 
 const createCard = (i) => {
